bd571ee2a1fa34b18c3b4e79dc283f3661899a87
Vinayaksharma17 <vvsharma999@gmail.com>
2026-03-21 14:12:44 +0530

    move all the constants into separate file

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,50 @@
+export const ITEMS_PER_PAGE = 12;
+
+export const STORAGE_KEYS = {
+  CART: "adiyogi_cart",
+  ADMIN_TOKEN: "adiyogi_admin_token",
+};
+
+export const PAYMENT_MODES = ["Credit", "Cash", "UPI", "Online"];
+
+export const INDIAN_STATES = [
+  "Andhra Pradesh",
+  "Arunachal Pradesh",
+  "Assam",
+  "Bihar",
+  "Chhattisgarh",
+  "Goa",
+  "Gujarat",
+  "Haryana",
+  "Himachal Pradesh",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttar Pradesh",
+  "Uttarakhand",
+  "West Bengal",
+  "Delhi",
+  "Puducherry",
+  "Chandigarh",
+  "Other",
+];
+
+export const NAV_LINKS = [
+  ["/", "Home"],
+  ["/#products", "Products"],
+  ["/#collections", "Collections"],
+  ["/#how-to-order", "How to Order"],
+];
